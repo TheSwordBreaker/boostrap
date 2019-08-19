dfb2607e0cdb354d261ad20befa28fff1e7fbec2
TheSwordBreaker <kunalguptadev007@gmail.com>
2019-08-19 20:13:16 +0530

    basic is complete

---
+++ myJQuery/myJQuery.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	
+	  
+
+    $('#effect').click(
+		function(){
+			$('#l').slideToggle(1000);
+		}
+	)
+	// $('P').click(function () {
+	//   console.log('you press is ', this);
+
+	// });
+
+	// $('#third').click(function () {
+	//   $(this).hide();
+	// }
+	// )
+
+	// $('.me').dblclick(
+	//   function () {
+	//     $('#third').show();
+	//   }
+	// )
+
+	// $('*').hover(
+	//   function(){
+	//     console.log("you enter :",this);
+	//   }
+	// )
+});
